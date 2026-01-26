0f0df16bd3885b7de2211fc2c230864fa6de0db5
FK <ntelgon@gmail.com>
2026-01-27 00:31:33 +0300

    seq update

--- verif/sequences/lib/mac_reg_config_seq.sv
+++ verif/sequences/lib/mac_reg_config_seq.sv
@@ -29,19 +29,19 @@ class mac_reg_config_seq extends uvm_sequence;
     wr_seq = axi_lite_write_seq::type_id::create("wr_seq");
     wr_seq.addr = MAC_CTRL_REG;
     wr_seq.data = {29'h0, promiscuous, enable_rx, enable_tx};
-    wr_seq.start(null);  // Uses parent sequencer
+    wr_seq.start(m_sequencer);  // Uses parent sequencer
     
     // Configure MAC address low
     wr_seq = axi_lite_write_seq::type_id::create("wr_seq");
     wr_seq.addr = MAC_ADDR_L_REG;
     wr_seq.data = mac_address[31:0];
-    wr_seq.start(null);
+    wr_seq.start(m_sequencer);
     
     // Configure MAC address high
     wr_seq = axi_lite_write_seq::type_id::create("wr_seq");
     wr_seq.addr = MAC_ADDR_H_REG;
     wr_seq.data = {16'h0, mac_address[47:32]};
-    wr_seq.start(null);
+    wr_seq.start(m_sequencer);
     
     `uvm_info("MAC_CFG", $sformatf("MAC configured: TX=%0b, RX=%0b, Addr=%0h", 
                                     enable_tx, enable_rx, mac_address), UVM_LOW)
